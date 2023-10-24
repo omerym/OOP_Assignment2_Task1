e304d11671a22e94d61964d2750319a8d43f9f8a
Omer Yassir <omerym10@gmail.com>
2023-10-24 18:39:32 +0300

    Add project files.

---
+++ OOP_Assignment2_Task1/OOP_Assignment2_Task1.cpp
@@ -0,0 +1,12 @@
+﻿// OOP_Assignment2_Task1.cpp : Defines the entry point for the application.
+//
+
+#include "OOP_Assignment2_Task1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ OOP_Assignment2_Task1/OOP_Assignment2_Task1.h
@@ -0,0 +1,8 @@
+﻿// OOP_Assignment2_Task1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
