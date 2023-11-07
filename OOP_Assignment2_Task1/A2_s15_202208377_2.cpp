84f64a9b48d98f23d1789dcf4239027fcd2bc27c
Op_Bashar <basharabdalla5@gmail.com>
2023-11-07 06:11:27 +0200

    يسشب

--- OOP_Assignment2_Task1/A2_s15_202208377_2.cpp
+++ OOP_Assignment2_Task1/A2_s15_202208377_2.cpp
@@ -1,46 +1,75 @@
-// File: A2_S15_20220837_2cpp
-// Purpose: .........
-// Author: bashar abdalla
-// Section: S15
-// ID: 20220837
-// TA: ........................
-// Date: 4 nov 2023
-
-
-#ifndef _2
-#define _2
-
 #include <iostream>
+#include <vector>
 #include <string>
+#include <cctype> 
+
 using namespace std;
-void Problem2() {
-    string sentence;
+
+int main() {
+    vector<string> strs;
+    string str;
+
+    // take input as line
+    getline(cin, str);
+
+    char separator = ' ';
+
+    // store every string chars in copy string
+    string copy;
     
-    
-    cout << "Enter a sentence: ";
-    getline(cin, sentence);
-    
-    
-   int founded = sentence.find("he");
-    while (founded != string::npos) {
-        sentence.replace(founded, 3,"he or she ");
-        founded = sentence.find("he", founded+8); 
+    for (int i = 0; i < str.size(); i++) {
+        // if char in str is " " then push back to vector as one string and clear it
+        if (str[i] == separator) {
+            strs.push_back(copy);
+            copy.clear();
+        }
+        // other wise will keep add char to string copy
+         else {
+            copy += str[i];
+        }
     }
 
-    founded = sentence.find("him");
-    while (founded != string::npos) {
-        sentence.replace(founded,  4,"him or her ");
-        founded = sentence.find("him", founded+10); 
+    // Push the last word into the vector (it may not end with a space)
+    strs.push_back(copy);
+
+    for (int i = 0; i < strs.size(); i++) {
+
+        // store current string size in curStrSize
+        int curStrSize = strs[i].size();
+
+        //  store string in the another string
+        string another = strs[i];
+
+        // to store last char in current string
+        char lastChar = another[another.size() - 1];
+
+        // delete last char in string another in case if the string was (him! or him? or him5) 
+        another.pop_back();
+
+        // first check if the string equal if "Him" or "him" if don't equal then if the string without last char ('.' or '?' or '5') if equal if will replace it other wise will continue wit doing anything
+        if (strs[i] == "Him" || strs[i] == "him" || (!isdigit(strs[i][curStrSize - 1]) && (another == "Him" || another == "him"))) {
+            if (strs[i] == "Him") {
+                strs[i] = "Him or her";
+            } else if (strs[i] == "him") {
+                strs[i] = "him or her";
+            } else {
+                strs[i] = "him or her";
+                strs[i].push_back(lastChar);
+            }
+        } else if (strs[i] == "He" || strs[i] == "he" || (!isdigit(strs[i][curStrSize - 1]) && (strs[i] == "He" || strs[i] == "he"))) {
+            if (strs[i] == "He") {
+                strs[i] = "He or she";
+            } else if (strs[i] == "he") {
+                strs[i] = "he or she";
+            } else {
+                strs[i] = "he or she";
+            }
+        }
     }
 
-    founded = sentence.find("his");
-    while (founded != string::npos) {
-        sentence.replace(founded,  4,"his or her ");
-        founded = sentence.find("his", founded +10); 
+    for (int i = 0; i < strs.size(); i++) {
+        cout << strs[i] << " ";
     }
 
-    
-   cout << "new sentence: " << sentence << endl;
-
+    return 0;
 }
-#endif
